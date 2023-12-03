421048c72043be60d91a13820577663710ddeca9
zanderdo <adobm001@ucr.edu>
2023-12-03 03:47:43 -0800

    Refactored code to make repeat runs easier and made orbital injection time based

--- kAscent_Guidance.py
+++ kAscent_Guidance.py
@@ -3,6 +3,12 @@
 import krpc
 from time import sleep
 
+tgtApo = 85000
+topGravityTurn = 7000
+numSeparatrons = 0
+timeBeforeApo = 25
+
+
 
 def countdown ():
     print("Launching in:\n")
@@ -38,49 +44,32 @@ def initial_ascent(ap):
     hdg_target = 90
     ap.target_pitch_and_heading(pitch_target, hdg_target)
 
-def mid_ascent(ap):
+def gravity_turn(ap):
     hdg_target = 90
-    pitch_target = 80 - (35 * (vessel.flight().mean_altitude / 9500))
+    pitch_target = 80 - (35 * (vessel.flight().mean_altitude / topGravityTurn))
     ap.target_pitch_and_heading(pitch_target, hdg_target)
-
 
 def high_ascent(ap):
-    hdg_target = 90
-    pitch_target = 45 - (45 * (vessel.flight().mean_altitude / 45000))
-    ap.target_pitch_and_heading(pitch_target, hdg_target)
-
-def orbital_injection(ap):
     ap.target_direction = (0,1,0)
 
-def circularization(ap, circAlt, currResources, currStage):
-    while abs(vessel.orbit.apoapsis_altitude - vessel.orbit.periapsis_altitude > 1000):
+def orbital_injection(ap, circAlt):
         ap.target_direction = (0,1,0)
-        if (vessel.flight().mean_altitude < circAlt):
+        if (vessel.orbit.time_to_apoapsis > timeBeforeApo):
             set_throttle(0)
         elif (abs(vessel.orbit.apoapsis_altitude - vessel.orbit.periapsis_altitude > 1000)):
             set_throttle(100)
         else:
             set_throttle(0)
-        currStage = check_stage(currResources, currStage)
-        currResources = vessel.resources_in_decouple_stage(currStage, False)
 
 
 def check_stage(currResources, currStage):
-    print(currResources.amount("LiquidFuel"))
-    print(currStage)
     if (currResources.has_resource("SolidFuel")):
-        if (currResources.amount("SolidFuel") <= 49):
-            print(currResources.amount("SolidFuel"))
+        if (currResources.amount("SolidFuel") <= 1 + (numSeparatrons * 8)):
             stage()
             currStage = currStage - 1
-            print("CURRSTAGE: ")
-            print(currStage)
             currResources = vessel.resources_in_decouple_stage(currStage, False)
-    print(currStage)
-
     if (currResources.has_resource("LiquidFuel")):
         if (currResources.amount("LiquidFuel") <= 1):
-            print(currResources.amount("LiquidFuel"))
             stage()
             currStage = currStage - 1
             currResources = vessel.resources_in_decouple_stage(currStage, False)
@@ -91,6 +80,10 @@ conn = krpc.connect(name="krpc connected")    #connect to krpc server
 vessel = conn.space_center.active_vessel      #connect to active vessel
 print("Current Vessel: ")
 print(vessel.name)                            #print current vessel info
+print("Please enter the number of separatrons on the rocket: ")
+userInput = input()
+numSeparatrons = int(userInput)
+print(numSeparatrons)
 print("Enter Y to Launch Rocket")
 
 userInput = ""
@@ -112,28 +105,24 @@ init_rocket(ap)
 countdown()
 stage()
 sleep(0.01)
-while vessel.flight().mean_altitude <= 1000:          #keep vessel 80 pitch 90 hdg until 1000m
-    initial_ascent(ap)
 
-while vessel.flight().mean_altitude <= 9500:         #reach 45 pitch 90 heading at 11000m
-    mid_ascent(ap)
+while 1:
+    if (vessel.flight().mean_altitude <= 500):
+        print("ia")
+        initial_ascent(ap)
+    elif (vessel.flight().mean_altitude <= topGravityTurn and vessel.flight().mean_altitude >500):
+        print("gt")
+        gravity_turn(ap)
+    elif (vessel.orbit.apoapsis_altitude <= tgtApo and vessel.flight().mean_altitude > topGravityTurn):
+        print("ha")
+        if (ap.reference_frame != vessel.orbital_reference_frame):
+            ap.reference_frame = vessel.orbital_reference_frame
+        high_ascent(ap)
+    elif (vessel.orbit.apoapsis_altitude > tgtApo - 1000):
+        print("oi")
+        if (ap.reference_frame != vessel.orbital_reference_frame):
+            ap.reference_frame = vessel.orbital_reference_frame
+        orbital_injection(ap, tgtApo)
     currStage = check_stage(currResources, currStage)
-
-
-
-while vessel.flight().mean_altitude <= 38000:
-    high_ascent(ap)
-    currStage = check_stage(currResources, currStage)
-
-ap.reference_frame = vessel.orbital_reference_frame
-
-while vessel.orbit.apoapsis_altitude <= 85000:
-    orbital_injection(ap)
-    currStage = check_stage(currResources, currStage)
-
-set_throttle(0)
-
-circAlt = 84000
-
-
-circularization(ap, circAlt, currResources, currStage)
+    currResources = currResources = vessel.resources_in_decouple_stage(currStage, False)
+    sleep(0.01)
