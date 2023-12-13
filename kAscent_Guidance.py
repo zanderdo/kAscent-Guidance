1639b8e15c28c726ba75fd7261e8a7c62f75293b
zanderdo <adobm001@ucr.edu>
2023-12-13 01:55:56 -0800

    Added calc_burn function to calculate time before apoapsis to begin burn.  Currently sets time to 0.

--- kAscent_Guidance.py
+++ kAscent_Guidance.py
@@ -1,12 +1,22 @@
 
 #version 0.01
 import krpc
+import math
 from time import sleep
 
 tgtApo = 85000
 topGravityTurn = 7000
 numSeparatrons = 0
 timeBeforeApo = 25
+actualApoAlt = 0
+velAtApo = 0
+xVel = 0
+gravConst = 6.67 * pow(10, -11)
+mKerbin = 5.29 * pow(10, 22)
+rKerbin = 600000
+timeBurnStartBeforeApo = 0
+hasCalced = False
+
 
 
 
@@ -54,12 +64,42 @@ def high_ascent(ap):
 
 def orbital_injection(ap, circAlt):
         ap.target_direction = (0,1,0)
-        if (vessel.orbit.time_to_apoapsis > timeBeforeApo):
+        if (vessel.orbit.time_to_apoapsis > timeBurnStartBeforeApo):
             set_throttle(0)
         elif (abs(vessel.orbit.apoapsis_altitude - vessel.orbit.periapsis_altitude > 1000)):
             set_throttle(100)
         else:
             set_throttle(0)
+
+def calc_burn(hasCalced):
+    if not hasCalced:
+        actualApoAlt = vessel.orbit.apoapsis
+        calcSurfRefFrame = vessel.orbit.body.reference_frame
+        apoRadius = vessel.orbit.apoapsis_altitude + rKerbin
+        proAngle = math.asin(vessel.flight(calcSurfRefFrame).vertical_speed / vessel.orbit.speed)
+        xVel = vessel.orbit.speed * math.cos(proAngle)
+        velAtApo = xVel
+        mShip = vessel.mass
+
+        forceKerbin = gravConst * ((mShip * mKerbin) / pow(apoRadius, 2))
+        accelerationAtApo = forceKerbin / mShip
+        vOrbit = accelerationAtApo * apoRadius
+
+        totalThrust = 0
+        activeEngines = [e for e in vessel.parts.engines if e.active and e.has_fuel]
+        totalThrust = sum(engine.max_thrust for engine in activeEngines)
+        initAcceleration = totalThrust / vessel.mass
+        print("total thrust: ")
+        print(totalThrust)
+        print("mass: ")
+        print(vessel.mass)
+        print("init acceleration: ")
+        print(initAcceleration)
+        desiredChange = vOrbit - velAtApo
+        approxTimeBurn = desiredChange / initAcceleration
+        timeBurnStartBeforeApo = approxTimeBurn / 2
+        hasCalced = True
+
 
 
 def check_stage(currResources, currStage):
@@ -108,18 +148,17 @@ sleep(0.01)
 
 while 1:
     if (vessel.flight().mean_altitude <= 500):
-        print("ia")
         initial_ascent(ap)
     elif (vessel.flight().mean_altitude <= topGravityTurn and vessel.flight().mean_altitude >500):
-        print("gt")
         gravity_turn(ap)
     elif (vessel.orbit.apoapsis_altitude <= tgtApo and vessel.flight().mean_altitude > topGravityTurn):
-        print("ha")
         if (ap.reference_frame != vessel.orbital_reference_frame):
             ap.reference_frame = vessel.orbital_reference_frame
         high_ascent(ap)
-    elif (vessel.orbit.apoapsis_altitude > tgtApo - 1000):
-        print("oi")
+    elif (vessel.orbit.apoapsis_altitude > 70000):
+        if (not hasCalced and vessel.flight(vessel.surface_reference_frame).mean_altitude > 70000):
+            calc_burn(hasCalced)
+            hasCalced = True
         if (ap.reference_frame != vessel.orbital_reference_frame):
             ap.reference_frame = vessel.orbital_reference_frame
         orbital_injection(ap, tgtApo)
